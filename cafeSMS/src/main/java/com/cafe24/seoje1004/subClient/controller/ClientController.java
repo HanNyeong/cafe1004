2d30968fcf22a4218fdcb7566e09da7344e2b791
sexyhyo <sexy_hyo@naver.com>
2016-07-28 18:31:05 +0900

    [0728/박효민]SubClient controller,service,model,repository,jsp file생성 view/sub/subClient 폴더 생성, controller,service,dao 애노테이션추가

---
+++ cafeSMS/src/main/java/com/cafe24/seoje1004/subClient/controller/ClientController.java
@@ -0,0 +1,8 @@
+package com.cafe24.seoje1004.subClient.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ClientController {
+
+}
---
+++ cafeSMS/src/main/java/com/cafe24/seoje1004/subClientService/ClientService.java
@@ -0,0 +1,5 @@
+package com.cafe24.seoje1004.subClientService;
+
+public class ClientService {
+
+}
