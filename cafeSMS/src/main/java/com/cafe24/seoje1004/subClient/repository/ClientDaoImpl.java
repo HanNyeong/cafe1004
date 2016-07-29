1ac9204e48ccf1848eecb095c863a08e7508578b
hyo <phm7033@gmail.com>
2016-07-29 11:11:24 +0900

    [0729/박효민] ClientList출력작업(controller,service,Dao,Mapper)

--- cafeSMS/src/main/java/com/cafe24/seoje1004/subClient/repository/ClientDaoImpl.java
+++ cafeSMS/src/main/java/com/cafe24/seoje1004/subClient/repository/ClientDaoImpl.java
@@ -1,8 +1,26 @@
 package com.cafe24.seoje1004.subClient.repository;
 
+import java.util.List;
+import java.util.Map;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.cafe24.seoje1004.subClient.model.Client;
+
 @Repository
-public class ClientDaoImpl {
+public class ClientDaoImpl implements ClientDao{
+	
+	@Autowired
+	private SqlSessionTemplate sqlSessionSubClient;
+	
+	private final String NS = "org.ksmart02.cafe.subClient.repository.ClientMapper";
+	
+	@Override
+	public List<Client> viewClientList(Map<String, Object> map) {
+		System.out.println("repository//viewClientList실행");
+		return sqlSessionSubClient.selectList(NS+".selectSubClient", map);
+	}
 
 }
