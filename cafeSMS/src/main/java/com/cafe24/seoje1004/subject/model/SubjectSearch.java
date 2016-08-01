9103373cab706077d792044a953329217e324cba
hyo <phm7033@gmail.com>
2016-08-01 11:25:31 +0900

    [0801박효민] subject/jsp,model추가

--- cafeSMS/src/main/java/com/cafe24/seoje1004/subject/model/SubjectSearch.java
+++ cafeSMS/src/main/java/com/cafe24/seoje1004/subject/model/SubjectSearch.java
@@ -1,5 +1,54 @@
 package com.cafe24.seoje1004.subject.model;
 
 public class SubjectSearch {
-
+	private String upDown;
+	private String criteria;
+	private String regitDateStart;
+	private String regitDateEnd;
+	private String searchKey;
+	private String searchSubject;
+	public String getUpDown() {
+		return upDown;
+	}
+	public void setUpDown(String upDown) {
+		this.upDown = upDown;
+	}
+	public String getCriteria() {
+		return criteria;
+	}
+	public void setCriteria(String criteria) {
+		this.criteria = criteria;
+	}
+	public String getRegitDateStart() {
+		return regitDateStart;
+	}
+	public void setRegitDateStart(String regitDateStart) {
+		this.regitDateStart = regitDateStart;
+	}
+	public String getRegitDateEnd() {
+		return regitDateEnd;
+	}
+	public void setRegitDateEnd(String regitDateEnd) {
+		this.regitDateEnd = regitDateEnd;
+	}
+	public String getSearchKey() {
+		return searchKey;
+	}
+	public void setSearchKey(String searchKey) {
+		this.searchKey = searchKey;
+	}
+	public String getSearchSubClient() {
+		return searchSubject;
+	}
+	public void setSearchSubClient(String searchSubject) {
+		this.searchSubject = searchSubject;
+	}
+	@Override
+	public String toString() {
+		return "SubjectSearch [upDown=" + upDown + ", criteria=" + criteria + ", regitDateStart=" + regitDateStart
+				+ ", regitDateEnd=" + regitDateEnd + ", searchKey=" + searchKey + ", searchSubject=" + searchSubject
+				+ "]";
+	}
+	
+	
 }
